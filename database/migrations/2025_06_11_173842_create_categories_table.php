92ef35e01585b0eb767bfc3258f14d9d15654318
Victorpurcell <victordpurcell25@gmail.com>
2025-06-11 14:57:14 -0300

    first commit

---
+++ database/migrations/2025_06_11_173842_create_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->boolean('is_global')->default(false); // true = categoria predefinida
+            $table->foreignId('user_id')->nullable()->constrained()->onDelete('cascade'); // se for personalizada
+            $table->timestamps();
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('categories');
+    }
+};
